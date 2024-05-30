0a5cbd6ab4618167c474c4a4c6e51930d388dc49
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2024-05-30 17:14:07 +0200

    fix: remove skia value deps

--- src/utils/unwrap-animated-value.ts
+++ src/utils/unwrap-animated-value.ts
@@ -1,33 +1,17 @@
-import type { SkiaValue } from '@shopify/react-native-skia';
-import type Animated from 'react-native-reanimated';
+import { SharedValue } from 'react-native-reanimated';
 
-type SkiaValueWithSelector<T> = {
-  value: SkiaValue<T>;
-  selector: (v: T) => T;
-};
-const unwrapAnimatedValue = <T>(
-  value: SkiaValue<T> | SkiaValueWithSelector<T> | Animated.SharedValue<T> | T
-): T => {
+const unwrapAnimatedValue = <T>(value: SharedValue<T> | T): T => {
   'worklet';
 
-  if ((value as Animated.SharedValue<T>).value != null) {
-    return (value as Animated.SharedValue<T>).value as T;
-  }
-
-  if ((value as SkiaValue<T>).current != null) {
-    return (value as SkiaValue<T>).current;
-  }
-
-  if ((value as SkiaValueWithSelector<T>).selector != null) {
-    const skiaValueWithSelector = value as SkiaValueWithSelector<T>;
-    return skiaValueWithSelector.selector(skiaValueWithSelector.value.current);
+  if ((value as SharedValue<T>).value != null) {
+    return (value as SharedValue<T>).value as T;
   }
 
   return value as T;
 };
 
 const unwrapAnimatedValueObject = <T>(
-  value: Record<any, SkiaValue<T> | T>
+  value: Record<any, SharedValue<T> | T>
 ): Record<any, T> => {
   'worklet';
   return Object.keys(value).reduce((acc, key) => {
