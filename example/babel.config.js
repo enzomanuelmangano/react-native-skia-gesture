09aaf091afa04918ed74db1bb1fb7ba601ca4d14
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2025-09-22 23:11:03 +0200

    chore: enable Babel cache for improved build performance

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = function (api) {
-  api.cache(false);
+  api.cache(true);
 
   return {
     presets: ['babel-preset-expo'],
